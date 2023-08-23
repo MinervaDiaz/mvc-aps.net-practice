6832d89a22933c2da7e99fd7ddba50a215bd486c
MinervaD <minervadiago@gmail.com>
2023-08-22 19:00:16 -0600

    crud de las 4 tablas CHECK, el sweet alert no funciona y el diseño no está listo

--- Models/ViewModels/NuevaFoto.cs
+++ Models/ViewModels/NuevaFoto.cs
@@ -13,7 +13,7 @@ namespace TurismoMexicoMVC.Models.ViewModels
         [Required]//datavalidator   EL VALOR DEBAJO DEL REQUIRED NO DEBE IR NULO
         [Display(Name = "lugar_id")]
         public int lugar_id { get; set; }
-        [Required]//datavalidator   EL VALOR DEBAJO DEL REQUIRED NO DEBE IR NULO
+        [Required]
         [Display(Name = "url_foto")]
         public string url_foto { get; set; }
         public string descripcion { get; set; }
